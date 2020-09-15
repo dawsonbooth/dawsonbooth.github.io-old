675836215d010ec8d3f8d7a90d7363a940f984f0
dawsonbooth <github@dawsonbooth.com>
2020-09-15 17:54:59 -0500

    Use icons for social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,64 +1,82 @@
 import * as React from "react";
 import { Flex, Text } from "rebass";
+import {
+  ExpoIcon,
+  GitHubIcon,
+  LinkedInIcon,
+  FacebookIcon,
+  TwitterIcon,
+  InstagramIcon,
+  SpotifyIcon,
+  MailIcon,
+} from "./icons";
 
-import Link from "./Link";
+import Link, { LinkProps } from "./Link";
 
-export default () => { // TODO: Extract sections and change social names to icons
-  return (
+const IconNavLink: React.FC<LinkProps> = (props) => (
+  <Link variant="nav" {...props} width={40} height={40} />
+);
+
+const Copyright = () => (
+  <Flex flex={1} justifyContent="center">
+    <Text p={2} mr="auto">
+      &#169; Copyright 2020 Dawson Booth
+    </Text>
+  </Flex>
+);
+
+const Social = () => (
+  <Flex flex={1} justifyContent="center">
+    <IconNavLink href="https://expo.io/@dawsonbooth">
+      <ExpoIcon />
+    </IconNavLink>
+    <IconNavLink href="https://github.com/dawsonbooth/">
+      <GitHubIcon />
+    </IconNavLink>
+    <IconNavLink href="https://www.linkedin.com/in/dawsonbooth/">
+      <LinkedInIcon />
+    </IconNavLink>
+    <IconNavLink href="https://www.facebook.com/dawsonbooth/">
+      <FacebookIcon />
+    </IconNavLink>
+    <IconNavLink href="https://twitter.com/dawsonbooth_">
+      <TwitterIcon />
+    </IconNavLink>
+    <IconNavLink href="https://instagram.com/dawsonbooth">
+      <InstagramIcon />
+    </IconNavLink>
+    <IconNavLink href="https://open.spotify.com/user/12155422298">
+      <SpotifyIcon />
+    </IconNavLink>
+    <IconNavLink href="mailto:contact@dawsonbooth.com">
+      <MailIcon />
+    </IconNavLink>
+  </Flex>
+);
+
+const MadeWith = () => (
+  <Flex flex={1} justifyContent="center">
+    <Text ml="auto">
+      Made with&nbsp;
+      <Link href="https://reactjs.org/">React</Link>,&nbsp;
+      <Link href="https://www.gatsbyjs.com/">Gatsby</Link>,&nbsp;
+      <Link href="https://rebassjs.org/">Rebass</Link>, and&nbsp;
+      <Link href="https://theme-ui.com/">Theme UI</Link>
+    </Text>
+  </Flex>
+);
+
+export default () => (
+  <Flex as="footer" justifyContent="center" py={2} color="background" bg="text">
     <Flex
-      as="header"
-      justifyContent="center"
-      py={2}
-      color="background"
-      bg="text"
+      justifyContent="space-between"
+      alignItems="center"
+      width="100%"
+      maxWidth={2048}
     >
-      <Flex
-        justifyContent="space-between"
-        alignItems="center"
-        width="100%"
-        maxWidth={2048}
-      >
-        <Flex flex={1} justifyContent="center">
-          <Text p={2} mr="auto">
-            &#169; Copyright 2020 Dawson Booth
-          </Text>
-        </Flex>
-        <Flex flex={1} justifyContent="center">
-          <Link variant="nav" href="https://expo.io/@dawsonbooth">
-            Expo
-          </Link>
-          <Link variant="nav" href="https://github.com/dawsonbooth/">
-            GitHub
-          </Link>
-          <Link variant="nav" href="https://www.linkedin.com/in/dawsonbooth/">
-            LinkedIn
-          </Link>
-          <Link variant="nav" href="https://www.facebook.com/dawsonbooth/">
-            Facebook
-          </Link>
-          <Link variant="nav" href="https://twitter.com/dawsonbooth_">
-            Twitter
-          </Link>
-          <Link variant="nav" href="https://instagram.com/dawsonbooth">
-            Instagram
-          </Link>
-          <Link variant="nav" href="https://open.spotify.com/user/12155422298">
-            Spotify
-          </Link>
-          <Link variant="nav" href="mailto:contact@dawsonbooth.com">
-            Email
-          </Link>
-        </Flex>
-        <Flex flex={1} justifyContent="center">
-          <Text ml="auto">
-            Made with&nbsp;
-            <Link href="https://reactjs.org/">React</Link>,&nbsp;
-            <Link href="https://www.gatsbyjs.com/">Gatsby</Link>,&nbsp;
-            <Link href="https://rebassjs.org/">Rebass</Link>, and&nbsp;
-            <Link href="https://theme-ui.com/">Theme UI</Link>
-          </Text>
-        </Flex>
-      </Flex>
+      <Copyright />
+      <Social />
+      <MadeWith />
     </Flex>
-  );
-};
+  </Flex>
+);
