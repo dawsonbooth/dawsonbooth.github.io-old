1a09b28fbef4a6d854f705111c5882368cae7854
dawsonbooth <github@dawsonbooth.com>
2020-09-15 17:55:28 -0500

    Export prop types

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,7 +4,7 @@ import { Link, LinkProps as RebassLinkProps } from "rebass";
 import GatsbyLink, { GatsbyLinkProps } from "gatsby-link";
 
 type Optional<T, K extends keyof T> = Omit<T, K> & Partial<T>;
-type LinkProps = RebassLinkProps & Optional<GatsbyLinkProps<object>, "to">;
+export type LinkProps = RebassLinkProps & Optional<GatsbyLinkProps<object>, "to">;
 
 export default (props: LinkProps) => {
   if (props.hasOwnProperty("to")) return <Link as={GatsbyLink} {...props} />;
