8cb0dec541da11af1e105be65aca0cd7e1879f54
dawsonbooth <github@dawsonbooth.com>
2020-09-15 17:55:45 -0500

    Extract icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,11 @@
 import * as React from "react";
-import { Box, Flex, Text } from "rebass";
+import { Box, Flex } from "rebass";
 import { useColorMode } from "theme-ui";
 
 import Link from "./Link";
+import { PaletteIcon } from "./icons";
 
 const modes = ["lite", "dark", "atlassian", "eva", "gray", "hack", "pink"];
-
-const PaletteIcon = () => (
-  <svg id="solid" viewBox="0 0 512 512">
-    <path
-      fill="currentcolor"
-      d="M425.705,86.587A238.427,238.427,0,0,0,256,16.292c-63.856,0-123.95,21.059-169.212,59.3C41.14,114.154,16,165.544,16,220.292c0,52.418,23.319,102.223,65.663,140.24,41.8,37.534,98.163,60.027,158.7,63.335a8.039,8.039,0,0,1,7.64,7.987V471.7a24.019,24.019,0,0,0,24.054,24q.873,0,1.747-.064a240.007,240.007,0,0,0,151.9-409.055ZM120,208a40,40,0,1,1,40-40A40,40,0,0,1,120,208Zm112-64a40,40,0,1,1,40-40A40,40,0,0,1,232,144Zm80-8a40,40,0,1,1,40,40A40,40,0,0,1,312,136Zm96,152a40,40,0,1,1,40-40A40,40,0,0,1,408,288Z"
-    />
-  </svg>
-);
 
 const PaletteButton = () => {
   const [mode, setMode] = useColorMode();
