18e28a9981bd462e3b138acd955b8107d79d64a7
dawsonbooth <github@dawsonbooth.com>
2020-11-03 22:18:44 -0600

    Remove copyright and made-with text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Flex, Text } from "rebass";
+import { Box, Flex } from "rebass";
 import {
   ExpoIcon,
   GitHubIcon,
@@ -17,71 +17,35 @@ const IconNavLink: React.FC<LinkProps> = (props) => (
   <Link variant="nav" {...props} width={40} height={40} />
 );
 
-const Copyright = () => (
-  <Flex flex={1} justifyContent="center">
-    <Text mr="auto">&#169; Copyright 2020 Dawson Booth</Text>
-  </Flex>
-);
-
-const Social = () => (
-  <Flex flex={1} justifyContent="center">
-    <IconNavLink href="https://www.linkedin.com/in/dawsonbooth/">
-      <LinkedInIcon />
-    </IconNavLink>
-    <IconNavLink href="https://github.com/dawsonbooth/">
-      <GitHubIcon />
-    </IconNavLink>
-    <IconNavLink href="https://expo.io/@dawsonbooth">
-      <ExpoIcon />
-    </IconNavLink>
-    <IconNavLink href="https://www.facebook.com/dawsonbooth/">
-      <FacebookIcon />
-    </IconNavLink>
-    <IconNavLink href="https://twitter.com/dawsonbooth_">
-      <TwitterIcon />
-    </IconNavLink>
-    <IconNavLink href="https://instagram.com/dawsonbooth">
-      <InstagramIcon />
-    </IconNavLink>
-    <IconNavLink href="https://open.spotify.com/user/12155422298">
-      <SpotifyIcon />
-    </IconNavLink>
-    <IconNavLink href="mailto:contact@dawsonbooth.com">
-      <MailIcon />
-    </IconNavLink>
-  </Flex>
-);
-
-const MadeWith = () => (
-  <Flex flex={1} justifyContent="center">
-    <Text ml="auto">
-      Made with&nbsp;
-      <Link href="https://reactjs.org/">React</Link>,&nbsp;
-      <Link href="https://www.gatsbyjs.com/">Gatsby</Link>,&nbsp;
-      <Link href="https://rebassjs.org/">Rebass</Link>, and&nbsp;
-      <Link href="https://theme-ui.com/">Theme UI</Link>
-    </Text>
-  </Flex>
-);
-
-export default () => (
-  <Flex
-    as="footer"
-    justifyContent="center"
-    py={2}
-    px={2}
-    color="background"
-    bg="text"
-  >
-    <Flex
-      justifyContent="space-between"
-      alignItems="center"
-      width="100%"
-      maxWidth={2048}
-    >
-      <Copyright />
-      <Social />
-      <MadeWith />
+const Footer = () => (
+  <Box as="footer" width="100%" p={2} color="background" bg="text">
+    <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
+      <IconNavLink href="https://www.linkedin.com/in/dawsonbooth/">
+        <LinkedInIcon />
+      </IconNavLink>
+      <IconNavLink href="https://github.com/dawsonbooth/">
+        <GitHubIcon />
+      </IconNavLink>
+      <IconNavLink href="https://expo.io/@dawsonbooth">
+        <ExpoIcon />
+      </IconNavLink>
+      <IconNavLink href="https://www.facebook.com/dawsonbooth/">
+        <FacebookIcon />
+      </IconNavLink>
+      <IconNavLink href="https://twitter.com/dawsonbooth_">
+        <TwitterIcon />
+      </IconNavLink>
+      <IconNavLink href="https://instagram.com/dawsonbooth">
+        <InstagramIcon />
+      </IconNavLink>
+      <IconNavLink href="https://open.spotify.com/user/12155422298">
+        <SpotifyIcon />
+      </IconNavLink>
+      <IconNavLink href="mailto:contact@dawsonbooth.com">
+        <MailIcon />
+      </IconNavLink>
     </Flex>
-  </Flex>
+  </Box>
 );
+
+export default Footer;
