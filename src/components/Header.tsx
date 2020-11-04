e19eccaf67301b9791e6c2d5c1f3774640d74f30
dawsonbooth <github@dawsonbooth.com>
2020-11-03 22:19:37 -0600

    Remove extra flexbox component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,26 +5,18 @@ import { useColorMode } from "theme-ui";
 import Link from "./Link";
 import { PaletteIcon } from "./icons";
 
-const modes = ["lite", "dark", "atlassian", "eva", "gray", "hack", "pink"];
+const colorModes = ["lite", "dark", "atlassian", "eva", "gray", "hack", "pink"];
 
 const PaletteButton = () => {
-  const [mode, setMode] = useColorMode();
+  const [colorMode, setColorMode] = useColorMode();
 
-  const cycleMode = () => {
-    const i = (modes.indexOf(mode) + 1) % modes.length;
-    setMode(modes[i]);
+  const cycleColorMode = () => {
+    const i = (colorModes.indexOf(colorMode) + 1) % colorModes.length;
+    setColorMode(colorModes[i]);
   };
 
   return (
-    <Link
-      variant="nav"
-      onClick={cycleMode}
-      width={40}
-      height={40}
-      sx={{
-        cursor: "pointer",
-      }}
-    >
+    <Link variant="nav" onClick={cycleColorMode} width={40} height={40}>
       <PaletteIcon />
     </Link>
   );
@@ -41,24 +33,15 @@ const Navigation = () => (
   </Box>
 );
 
-export default () => {
+const Header = () => {
   return (
-    <Flex
-      as="header"
-      justifyContent="center"
-      py={2}
-      color="background"
-      bg="text"
-    >
-      <Flex
-        justifyContent="space-between"
-        alignItems="center"
-        width="100%"
-        maxWidth={2048}
-      >
+    <Box as="header" width="100%" p={2} color="background" bg="text">
+      <Flex justifyContent="space-between" alignItems="center">
         <Navigation />
         <PaletteButton />
       </Flex>
-    </Flex>
+    </Box>
   );
 };
+
+export default Header;
