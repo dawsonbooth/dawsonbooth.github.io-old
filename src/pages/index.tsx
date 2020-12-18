8fa66f920a548772f5bf8fcf458e68f63f9557f9
dawsonbooth <github@dawsonbooth.com>
2020-12-17 23:51:30 -0600

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,21 @@
 import * as React from "react";
 
-import { graphql } from "gatsby";
-
-import { Heading, Text } from "theme-ui";
+import { Heading } from "theme-ui";
 
 import Layout from "../components/layout";
 import Link from "../components/Link";
 
 import routes from "../routes";
 
-interface HomeProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string;
-      };
-    };
-  };
-}
-
-const Home = ({ data }: HomeProps) => {
+const Home = () => {
   return (
     <Layout title="Home">
-      <Heading>Hello world!</Heading>
-      <Text>
-        Welcome to my new <strong>{data.site.siteMetadata.title}</strong>.
-      </Text>
+      <Heading as="h1" sx={{ fontSize: 36, textAlign: "center" }}>
+        Hello world! I'm Dawson.
+      </Heading>
       <Link to={routes.PROJECTS}>Go to Projects</Link>
     </Layout>
   );
 };
-
-export const pageQuery = graphql`
-  query HomeQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
 
 export default Home;
